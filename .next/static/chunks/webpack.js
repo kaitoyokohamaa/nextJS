cc21313a65524dcb94129df1f160a6f5e2293ef0
yoko <s1A121700885@toyo.jp>
2021-01-12 18:00:25 +0900

    fix:ダイナミックimportで生じたエラーの解消

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "81d3de7e888857739d46";
+/******/ 	var hotCurrentHash = "bb99a3d396fefee7420b";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
