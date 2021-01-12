5d1d072585202fefebe6e8bf87c0997b8cf1d5c8
yoko <s1A121700885@toyo.jp>
2021-01-12 17:31:21 +0900

    learn:dynamic route

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "d60ab47abb8a9e521c68";
+/******/ 	var hotCurrentHash = "81d3de7e888857739d46";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
